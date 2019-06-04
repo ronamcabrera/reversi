d942f99522b043f65fc6663d8fc2229f0262a8c5
Rona Cabrera <ronamc@uci.edu>
2019-06-04 13:39:10 +0900

    Finish flipping logic

--- server.js
+++ server.js
@@ -676,15 +676,16 @@ io.sockets.on('connection', function (socket){
 						};
 		socket.emit('play_token_response', success_data);
 
-		/*fix error 18:31 in part 24*/
 		/* Execute the move */
 		if(color == 'white'){
 			game.board[row][column] = 'w';
+			flip_board('w',row,column,game.board);
 			game.whose_turn = 'black';
 			game.legal_moves = calculate_valid_moves('b', game.board);
 		}
 		else if(color == 'black'){
 			game.board[row][column] = 'b';
+			flip_board('b',row,column,game.board);
 			game.whose_turn = 'white';
 			game.legal_moves = calculate_valid_moves('w', game.board);
 		}
@@ -823,6 +824,44 @@ function calculate_valid_moves(who,board){
 	}
 
 	return valid;
+}
+
+function flip_line(who,dr,dc,r,c,board){
+	if((r+dr < 0) || (r+dr > 7)){
+		return false;
+	}
+	if((c+dc < 0) || (c+dc > 7)){
+		return false;
+	}
+	if(board[r+dr][c+dc] === ' '){
+		return false;
+	}
+	if(board[r+dr][c+dc] === who){
+		return true;
+	}
+	else{
+		if(flip_line(who,dr,dc,r+dr,c+dc,board)){
+			board[r+dr][c+dc] = who;
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+}
+
+function flip_board(who,row,column,board){
+	flip_line(who,-1,-1,row,column,board);
+	flip_line(who,-1, 0,row,column,board);
+	flip_line(who,-1, 1,row,column,board);
+
+	flip_line(who, 0,-1,row,column,board);
+	flip_line(who, 0, 1,row,column,board);
+
+	flip_line(who, 1,-1,row,column,board);
+	flip_line(who, 1, 0,row,column,board);
+	flip_line(who, 1, 1,row,column,board);
 }
 
 
