84303c4f840b328a3a5b02e16612fe1115d83b3b
Rona Cabrera <ronamc@uci.edu>
2019-06-04 13:23:53 +0900

    Allow certain valid moves part 1

--- public/js/main.js
+++ public/js/main.js
@@ -329,6 +329,7 @@ socket.on('game_update',function(payload){
 	}
 
 	$('#my_color').html('<h3 id="my_color">I am '+my_color+'</h3>')
+	$('#my_color').append('<h4>It is '+payload.game.whose_turn+'\'s turn</h4>');
 
 	/* Animate changes to the board */
 
@@ -376,9 +377,14 @@ socket.on('game_update',function(payload){
 				else{
 					$('#'+row+'_'+column).html('<img src="assets/images/error.gif" alt="error"/>');
 				}
-				/* Set up interactivity */
-				$('#'+row+'_'+column).off('click');
-				if(board[row][column] == ' '){
+
+			}
+
+			/* Set up interactivity */
+			$('#'+row+'_'+column).off('click');
+			$('#'+row+'_'+column).removeClass('hovered_over');
+			if(payload.game.whose_turn === my_color){
+				if(payload.game.legal_moves[row][column] === my_color.substr(0,1)){
 					$('#'+row+'_'+column).addClass('hovered_over');
 					$('#'+row+'_'+column).click(function(r,c){
 						return function(){
@@ -391,9 +397,6 @@ socket.on('game_update',function(payload){
 						};
 					}(row,column));
 				}
-				else{
-					$('#'+row+'_'+column).removeClass('hovered_over');
-				}
 			}
 		}
 	}
@@ -402,8 +405,7 @@ socket.on('game_update',function(payload){
 	$('#whitesum').html(whitesum);
 
 	old_board = board;
-
-}); 
+ });
 
 socket.on('play_token_response',function(payload){
 	console.log('*** Client Log Message: \'play_token_response\'\n\tpayload: '+JSON.stringify(payload));
